e2391432e2d5e9d249adc0a73e8b7de675286202
Daniil Chemaev <chemaev.ds@gmail.com>
2022-10-16 15:34:10 +0300

    Added basic login page

---
+++ src/main/java/ru/kpfu/itis/chemaev/net/servlet/LoginServlet.java
@@ -0,0 +1,33 @@
+package ru.kpfu.itis.chemaev.net.servlet;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.*;
+import java.io.IOException;
+
+@WebServlet(name = "loginServlet", urlPatterns = "/login")
+public class LoginServlet extends HttpServlet {
+
+    public static final String LOGIN = "admin";  // !TODO Сделать хранение логина и пароля в БД
+    public static final String PASSWORD = "admin";  // !TODO Хеширование пароля
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        resp.sendRedirect("login.html");  // !TODO Сделать отдельную страничку логина с ошибкой?
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        String login = req.getParameter("login");
+        String password = req.getParameter("password");
+
+        if (LOGIN.equals(login) && PASSWORD.equals(password)) {
+            HttpSession httpSession = req.getSession();
+            httpSession.setAttribute("username", login);
+            httpSession.setMaxInactiveInterval(60 * 60);
+
+            resp.sendRedirect("main.jsp");
+        } else {
+            resp.sendRedirect("/login");
+        }
+    }
+}
---
+++ src/main/webapp/base.ftl
@@ -0,0 +1,16 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title><@title></@title></title>
+</head>
+<body>
+
+<div id="header">
+    <h3> Header </h3>
+</div>
+
+<div class="content1">
+    <div class="content"><@content></@content></div>
+</div>
+</body>
+</html>
